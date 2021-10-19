978518924935c21c35b67504f3f8e24d2855f7fc
Michael <michael.spengler@sap.com>
2021-10-19 19:47:15 +0200

    prepare things

--- src/demo1.ts
+++ src/demo1.ts
@@ -1,9 +1,12 @@
 require('dotenv').config() // this ensures process.env. ... contains your .env file configuration values
-    
-const { DeFiService } = require("decentralized-finance-defi")
 
+const Web3 = require('web3');
+
+const web3 = new Web3(new Web3.providers.HttpProvider(process.env.PROVIDER_URL));
 const fromWalletAddress = process.env.SENDER_WALLET_ADDRESS
 const toWalletAddress = process.env.RECEIVER_WALLET_ADDRESS
 const amountInETH = 1
 const senderPrivateKey = process.env.SENDER_WALLET_PRIVATE_KEY
+
+console.log(`we will transfer ${amountInETH} ETH from ${fromWalletAddress} to ${toWalletAddress} - using privatekey 123 ;)`)
 
