dbc294e4255e2361dfa352de7e60226e2a42f1a9
Michael <michael.spengler@sap.com>
2021-10-20 21:52:27 +0200

    improve code-registry.sol

--- src/smart-contracts/code-registry.sol
+++ src/smart-contracts/code-registry.sol
@@ -5,9 +5,9 @@ contract CodeFileRepository {
     
     struct Entry { 
         string programmingLanguage;
-        string projectName;
-        string version;
+        string topicArea;
         string name;
+        string version;
         string content;
         string ethereumWalletAddressOfContributor;
     }
@@ -16,19 +16,20 @@ contract CodeFileRepository {
     Entry[] entries;
 
 
-    function addEntry(string memory programmingLanguage, string memory projectName, string memory version, string memory name, string memory content, string memory ethereumWalletAddressOfContributor) public {
+    // Example Input: 'TypeScript', 'tutorials','hello-world.ts','v0.0.1',`console.log('hello world :)')`, '0x7A915e362353d72570dcf90aa5BAA1C5B341c7AA'
+    function addEntry(string memory programmingLanguage, string memory topicArea, string memory name, string memory version, string memory content, string memory ethereumWalletAddressOfContributor) public {
         // might be needed: https://ethereum.stackexchange.com/questions/106801/how-to-code-a-string-replace-function-in-solidity
-        // Example Input: 'TypeScript', 'basics', 'v0.0.1', 'hello-world.ts', `console.log('hello world :)')`, '0x7A915e362353d72570dcf90aa5BAA1C5B341c7AA'
-        entries.push(Entry(programmingLanguage, projectName, version, name, content, ethereumWalletAddressOfContributor));
+        entries.push(Entry(programmingLanguage, topicArea, name, version, content, ethereumWalletAddressOfContributor));
     }
 
-    function getContent(string memory programmingLanguage, string memory projectName, string memory version, string memory name) public view returns (string memory) {
+    // example input: 'TypeScript','tutorials','hello-world.ts','v0.0.1'
+    function getContent(string memory programmingLanguage, string memory topicArea, string memory name, string memory version) public view returns (string memory) {
         
         for (uint i = 0; i < entries.length; i++) {
             if (compareStrings(entries[0].programmingLanguage, programmingLanguage) && 
-                compareStrings(entries[0].projectName, projectName) && 
-                compareStrings(entries[0].version, version) && 
-                compareStrings(entries[0].name, name)){
+                compareStrings(entries[0].topicArea, topicArea) && 
+                compareStrings(entries[0].name, name) && 
+                compareStrings(entries[0].version, version)){
                 
              return entries[0].content;            
              
